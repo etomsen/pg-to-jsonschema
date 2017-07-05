4d5e60a766d07d38e2fcfd47ebf3645bcdcbdbd3
Evgeny Tomsen <eugene.tomsen@gmail.com>
2017-07-05 18:51:22 +0200

    1.0.1

--- dist/src/sql.js
+++ dist/src/sql.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs = require("fs");
 exports.default = {
-    getTables: fs.readFileSync(__dirname + '../sql/tables.sql').toString(),
-    getColumns: fs.readFileSync(__dirname + '../sql/columns.sql').toString(),
-    getConstraints: fs.readFileSync(__dirname + '../sql/constraints.sql').toString()
+    getTables: fs.readFileSync(__dirname + '/../sql/tables.sql').toString(),
+    getColumns: fs.readFileSync(__dirname + '/../sql/columns.sql').toString(),
+    getConstraints: fs.readFileSync(__dirname + '/../sql/constraints.sql').toString()
 };
--- src/sql.ts
+++ src/sql.ts
@@ -1,7 +1,7 @@
 import * as fs from 'fs'
 
 export default {
-    getTables: fs.readFileSync(__dirname + '../sql/tables.sql').toString(),
-    getColumns: fs.readFileSync(__dirname + '../sql/columns.sql').toString(),
-    getConstraints: fs.readFileSync(__dirname + '../sql/constraints.sql').toString()
+    getTables: fs.readFileSync(__dirname + '/../sql/tables.sql').toString(),
+    getColumns: fs.readFileSync(__dirname + '/../sql/columns.sql').toString(),
+    getConstraints: fs.readFileSync(__dirname + '/../sql/constraints.sql').toString()
 }
